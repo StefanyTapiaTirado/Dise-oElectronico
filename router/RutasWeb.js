0567fd1bf0f650d3ffc089255fdb2967e34aa39f
gdgalvis <gdgalvis@uninorte.edu.co>
2021-09-20 23:23:03 -0500

    Commit German 2

--- router/RutasWeb.js
+++ router/RutasWeb.js
@@ -10,7 +10,11 @@ router.get('/', (req, res) => {
 router.get('/monitorear/tiemporeal', (req, res) => {
     res.render("tiemporeal")
 })
-router.get("/monitorear/tiemporeal/api", (req, res) => {
+router.get('/monitorear/consultas', (req, res) => {
+    res.render("consultas")
+})
+router.use(cors());
+router.get("/api", (req, res) => {
     //Obtener el ultimo dato de la base de datos
     conexion.query(`SELECT * FROM dataTaxi ORDER BY id DESC LIMIT 1`, (err, result) => {
         if (!err) {
@@ -21,9 +25,5 @@ router.get("/monitorear/tiemporeal/api", (req, res) => {
         }
     })
 })
-router.get('/monitorear/consultas', (req, res) => {
-    res.render("consultas")
-})
-router.use(cors());
 
 module.exports = router;
