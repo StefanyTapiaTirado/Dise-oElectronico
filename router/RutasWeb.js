73762756705e3be01b6db341471f9a66c72654c5
gdgalvis <73660713+gdgalvis@users.noreply.github.com>
2021-10-06 12:40:35 -0500

    Add files via upload

--- router/RutasWeb.js
+++ router/RutasWeb.js
@@ -12,7 +12,7 @@ router.get('/monitorear/tiemporeal', (req, res) => {
     res.render("tiemporeal")
 })
 router.get('/monitorear/consultas', (req, res) => {
-    res.render("consultas", {latlon:''})
+    res.render("consultas", {latlon:'', error:''})
 })
 router.use(cors());
 router.get("/monitorear/tiemporeal/api", (req, res) => {
@@ -29,26 +29,31 @@ router.get("/monitorear/tiemporeal/api", (req, res) => {
 router.post('/monitorear/consultas', (req, res) => {
     let idate =req.body.idate;
     let fdate =req.body.fdate;
-    let date1 = idate.split('T');
-    let date2 = fdate.split('T');
-    let hora1 = date1[1].toString();
-    let hora2 = date2[1].toString();
+    if (idate == '' || fdate == ''){
+      error = 'Todos los campos son requeridos para realizar la consulta.';
+      res.render("consultas", {error:error, latlon:''});
+    }else{
+      let date1 = idate.split('T');
+      let date2 = fdate.split('T');
+      let hora1 = date1[1].toString();
+      let hora2 = date2[1].toString();
+      
+      let time1 = date1[0] + ' ' + hora1 ;
+      let time2 = date2[0] + ' ' + hora2 ;
     
-    let time1 = date1[0] + ' ' + hora1 ;
-    let time2 = date2[0] + ' ' + hora2 ;
-
-    conexion.query(`SELECT * FROM dataTaxi WHERE (timestamps BETWEEN '${time1}' AND '${time2}')`, (err, result) => {
+      conexion.query(`SELECT * FROM dataTaxi WHERE (timestamps BETWEEN '${time1}' AND '${time2}')`, (err, result) => {
         if (!err) {
           let info = result;
           let latlon = Array(0);
           for (i=0;i<info.length;i++){
-              latlon[i] = [info[i]['latitud'],info[i]['longitud']];
+              latlon[i] = [info[i]['latitud'],info[i]['longitud'],info[i]['timestamps']];
           }
-          res.render("consultas", {latlon:latlon});
+          res.render("consultas", {error:'', latlon:latlon});
         } else {
           console.log(err);
         }
     })
+  }  
 })
 
 module.exports = router;
