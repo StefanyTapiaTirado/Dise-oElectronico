289f5faa199fa8737a286495894ccff35fc0f634
gdgalvis <73660713+gdgalvis@users.noreply.github.com>
2021-10-06 12:39:07 -0500

    Add files via upload

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const mysql = require('mysql');
 
 require('dotenv').config();
 
-var conexion = mysql.createConnection({
+const conexion = mysql.createConnection({
     host : process.env.HOST,
     database : process.env.DATABASE,
     user : process.env.USER,
