17e8a5657857fd0756f02ce4dd8856b26fb987dc
gdgalvis <73660713+gdgalvis@users.noreply.github.com>
2021-11-10 21:12:45 -0500

    Add files via upload

--- router/RutasWeb.js
+++ router/RutasWeb.js
@@ -12,7 +12,7 @@ router.get('/monitorear/tiemporeal', (req, res) => {
     res.render("tiemporeal")
 })
 router.get('/monitorear/consultas', (req, res) => {
-    res.render("consultas", {latlon:'', error:''})
+    res.render("consultas", {latlon:'',latlon2:'', error:''})
 })
 router.use(cors());
 router.get("/monitorear/tiemporeal/api/:id", (req, res) => {
@@ -48,11 +48,15 @@ router.post('/monitorear/consultas', (req, res) => {
       let time1 = date1[0] + ' ' + hora1 ;
       let time2 = date2[0] + ' ' + hora2 ;
       let tabledb = '';
+      let tabledb2 = '';
       //Se hace la consulta teniendo en cuenta ID
       if (id == 1){
         tabledb = 'dataTaxi';
       }else if (id == 2){
         tabledb = 'dataTaxi2';
+      }else if (id == 3){
+        tabledb = 'dataTaxi';
+        tabledb2 = 'dataTaxi2';
       }
 
       conexion.query(`SELECT * FROM ${tabledb} WHERE (timestamps BETWEEN '${time1}' AND '${time2}')`, (err, result) => {
@@ -62,11 +66,28 @@ router.post('/monitorear/consultas', (req, res) => {
           for (i=0;i<info.length;i++){
               latlon[i] = [info[i]['latitud'],info[i]['longitud'],info[i]['timestamps'],info[i]['rpm']];
           }
-          res.render("consultas", {error:'', latlon:latlon});
-        } else {
+          if (id != 3){
+            res.render("consultas", {error:'', latlon:latlon,latlon2:''});          
+          }
+          if (id == 3){
+          conexion.query(`SELECT * FROM ${tabledb2} WHERE (timestamps BETWEEN '${time1}' AND '${time2}')`, (err, result) => {
+            if (!err) {
+              let info2 = result;
+              let latlon2 = Array(0);
+              for (i=0;i<info2.length;i++){
+                  latlon2[i] = [info2[i]['latitud'],info2[i]['longitud'],info2[i]['timestamps'],info2[i]['rpm']];
+              }
+              res.render("consultas", {error:'', latlon:latlon,latlon2:latlon2});
+            } else {
+              console.log(err);
+            }
+          })
+          
+        } 
+        }else {
           console.log(err);
         }
-    })
+      })
   }  
 })
 
